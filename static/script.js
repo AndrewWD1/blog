a3b6c0afbe73cddedbb5ded1e4a0e31f6bf37d31
Andrew Doumont <andrew.doumont@gmail.com>
2022-04-08 14:09:10 -0600

    update

--- static/script.js
+++ static/script.js
@@ -1,14 +1,1 @@
-fetch("https://api.ipify.org?format=json")
-  .then(r => r.json())
-  .then(r => {
-    r.site = "blog"
-    fetch("https://whirlwindlightblueinteger.andrewdoumont.repl.co/target", {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify(r),
-    })
-  })
+console.log("Welcome to my site!")
